7d62f82cfa9d78574efe0a904c8693bf34d35d3d
unknown <diether.noche@gmail.com>
2025-08-10 12:15:34 +0800

    Add URL Params

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,29 @@
 $(document).ready(function () {
+  // Url Params
+  var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+      sURLVariables = sPageURL.split("&"),
+      sParameterName,
+      i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split("=");
+
+      if (sParameterName[0] === sParam) {
+        return sParameterName[1] === undefined
+          ? undefined
+          : decodeURIComponent(sParameterName[1]);
+      }
+    }
+    return undefined;
+  };
+
+  var fname = getUrlParameter("fname") || "";
+  $(".fname").text(fname);
+
+  var lname = getUrlParameter("lname") || "";
+  $(".lname").text(lname);
+
   // Dates
   const monthNames = [
     "January",
